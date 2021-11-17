0bc1f2a98ea46f7793be36250b07a28e7e8f13ad
Omar yulian <omar_052897@hotmail.com>
2021-11-16 22:29:26 -0500

    Se implementaron los modelos de Persona,Solicitud,Inmueble y Ciudad

---
+++ backend/src/models/inmueble.model.ts
@@ -0,0 +1,47 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Inmueble extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  direccion: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  valor: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  fotografia: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  videoYoutube: string;
+
+
+  constructor(data?: Partial<Inmueble>) {
+    super(data);
+  }
+}
+
+export interface InmuebleRelations {
+  // describe navigational properties here
+}
+
+export type InmuebleWithRelations = Inmueble & InmuebleRelations;
---
+++ backend/src/models/persona.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Persona extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombres: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  apellidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @property({
+    type: 'string',
+  })
+  clave?: string;
+
+
+  constructor(data?: Partial<Persona>) {
+    super(data);
+  }
+}
+
+export interface PersonaRelations {
+  // describe navigational properties here
+}
+
+export type PersonaWithRelations = Persona & PersonaRelations;
